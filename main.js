add032be735ff8f5bf536318fd6c826cd22cb5ca
l3ehfar <behnooshmzd@gmail.com>
2019-05-07 21:41:42 +0430

    debugged

--- main.js
+++ main.js
@@ -1,53 +1,46 @@
-function mainShow(n) {
+function mainShow() {
   stroke(100);
-  strokeWeight(10);
+  strokeWeight(1);
   noFill();
   push();
   translate(width / 2, height / 2);
-  rotate(15);
-  polygon(0, 0, n);
+  morph.morphShow();
   pop();
   push();
   translate(width / 2, height / 2 - (sin(60) * 25.882 * 4 + 25.882 * 2));
-  rotate(15);
-  polygon(0, 0, n);
+  morph.morphShow();
   pop();
   push();
   translate(width / 2, height / 2 + (sin(60) * 25.882 * 4 + 25.882 * 2));
-  rotate(15);
-  polygon(0, 0, n);
+  morph.morphShow();
   pop();
   push();
   translate(
     width / 2 + (sin(60) * 25.882 * 4 + 25.882 * 2) * sin(60),
     height / 2 + (sin(60) * 25.882 * 4 + 25.882 * 2) * sin(30)
   );
-  rotate(15);
-  polygon(0, 0, n);
+  morph.morphShow();
   pop();
   push();
   translate(
     width / 2 - (sin(60) * 25.882 * 4 + 25.882 * 2) * sin(60),
     height / 2 + (sin(60) * 25.882 * 4 + 25.882 * 2) * sin(30)
   );
-  rotate(15);
-  polygon(0, 0, n);
+  morph.morphShow();
   pop();
   push();
   translate(
     width / 2 - (sin(60) * 25.882 * 4 + 25.882 * 2) * sin(60),
     height / 2 - (sin(60) * 25.882 * 4 + 25.882 * 2) * sin(30)
   );
-  rotate(15);
-  polygon(0, 0, n);
+  morph.morphShow();
   pop();
   push();
   translate(
     width / 2 + (sin(60) * 25.882 * 4 + 25.882 * 2) * sin(60),
     height / 2 - (sin(60) * 25.882 * 4 + 25.882 * 2) * sin(30)
   );
-  rotate(15);
-  polygon(0, 0, n);
+  morph.morphShow();
   pop();
 
   push();
@@ -55,32 +48,28 @@ function mainShow(n) {
     width / 2 + 2 * (sin(60) * 25.882 * 4 + 25.882 * 2) * sin(60),
     height / 2 - 2 * (sin(60) * 25.882 * 4 + 25.882 * 2) * sin(30)
   );
-  rotate(15);
-  polygon(0, 0, n);
+  morph.morphShow();
   pop();
   push();
   translate(
     width / 2 - 2 * (sin(60) * 25.882 * 4 + 25.882 * 2) * sin(60),
     height / 2 - 2 * (sin(60) * 25.882 * 4 + 25.882 * 2) * sin(30)
   );
-  rotate(15);
-  polygon(0, 0, n);
+  morph.morphShow();
   pop();
   push();
   translate(
     width / 2 + 2 * (sin(60) * 25.882 * 4 + 25.882 * 2) * sin(60),
     height / 2 + 2 * (sin(60) * 25.882 * 4 + 25.882 * 2) * sin(30)
   );
-  rotate(15);
-  polygon(0, 0, n);
+  morph.morphShow();
   pop();
   push();
   translate(
     width / 2 - 2 * (sin(60) * 25.882 * 4 + 25.882 * 2) * sin(60),
     height / 2 + 2 * (sin(60) * 25.882 * 4 + 25.882 * 2) * sin(30)
   );
-  rotate(15);
-  polygon(0, 0, n);
+  morph.morphShow();
   pop();
 
   push();
@@ -88,32 +77,28 @@ function mainShow(n) {
     width / 2 - (sin(60) * 25.882 * 4 + 25.882 * 2) * sin(60),
     height / 2 + 3 * (sin(60) * 25.882 * 4 + 25.882 * 2) * sin(30)
   );
-  rotate(15);
-  polygon(0, 0, n);
+  morph.morphShow();
   pop();
   push();
   translate(
     width / 2 + (sin(60) * 25.882 * 4 + 25.882 * 2) * sin(60),
     height / 2 + 3 * (sin(60) * 25.882 * 4 + 25.882 * 2) * sin(30)
   );
-  rotate(15);
-  polygon(0, 0, n);
+  morph.morphShow();
   pop();
   push();
   translate(
     width / 2 - (sin(60) * 25.882 * 4 + 25.882 * 2) * sin(60),
     height / 2 - 3 * (sin(60) * 25.882 * 4 + 25.882 * 2) * sin(30)
   );
-  rotate(15);
-  polygon(0, 0, n);
+  morph.morphShow();
   pop();
   push();
   translate(
     width / 2 + (sin(60) * 25.882 * 4 + 25.882 * 2) * sin(60),
     height / 2 - 3 * (sin(60) * 25.882 * 4 + 25.882 * 2) * sin(30)
   );
-  rotate(15);
-  polygon(0, 0, n);
+  morph.morphShow();
   pop();
 
   push();
@@ -121,31 +106,27 @@ function mainShow(n) {
     width / 2 + 2 * (sin(60) * 25.882 * 4 + 25.882 * 2) * sin(60),
     height / 2
   );
-  rotate(15);
-  polygon(0, 0, n);
+  morph.morphShow();
   pop();
   push();
   translate(
     width / 2 - 2 * (sin(60) * 25.882 * 4 + 25.882 * 2) * sin(60),
     height / 2
   );
-  rotate(15);
-  polygon(0, 0, n);
+  morph.morphShow();
   pop();
   push();
   translate(
     width / 2,
     height / 2 - 4 * (sin(60) * 25.882 * 4 + 25.882 * 2) * sin(30)
   );
-  rotate(15);
-  polygon(0, 0, n);
+  morph.morphShow();
   pop();
   push();
   translate(
     width / 2,
     height / 2 + 4 * (sin(60) * 25.882 * 4 + 25.882 * 2) * sin(30)
   );
-  rotate(15);
-  polygon(0, 0, n);
+  morph.morphShow();
   pop();
 }
