9b0a9f582154e61063abe23ce065fef0e5fcc7b8
l3ehfar <behnooshmzd@gmail.com>
2019-05-12 00:09:39 +0430

    jigsaw falling into place?

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var z1, z2, z3, z4, z5, z6, z7, z8, z9;
+var z1, z2, z3, z4, z5, z6, z7, z8, z9, z10, z11, z12, z13, z14, z15;
 
 function mainShow(n) {
   stroke(200);
@@ -90,34 +90,15 @@ function mainShow(n) {
       stroke(z2);
     }
   }
-  push();
-  translate(width / 2 + w, height / 2 + h);
-  if (n == 24) {
-    rotate(15);
+  for (var i = -5; i <= 5; i += 2) {
+    push();
+    translate(width / 2 + w, height / 2 + i * h);
+    if (n == 24) {
+      rotate(15);
+    }
+    polygon(0, 0, n);
+    pop();
   }
-  polygon(0, 0, n);
-  pop();
-  push();
-  translate(width / 2 + w, height / 2 - 3 * h);
-  if (n == 24) {
-    rotate(15);
-  }
-  polygon(0, 0, n);
-  pop();
-  push();
-  translate(width / 2 + w, height / 2 - h);
-  if (n == 24) {
-    rotate(15);
-  }
-  polygon(0, 0, n);
-  pop();
-  push();
-  translate(width / 2 + w, height / 2 + 3 * h);
-  if (n == 24) {
-    rotate(15);
-  }
-  polygon(0, 0, n);
-  pop();
 
   stroke(30);
   if (dist(mouseX, 0, width / 2 - w, 0) <= 100 * sin(60)) {
@@ -129,34 +110,15 @@ function mainShow(n) {
       stroke(z3);
     }
   }
-  push();
-  translate(width / 2 - w, height / 2 + h);
-  if (n == 24) {
-    rotate(15);
+  for (var i = -5; i <= 5; i += 2) {
+    push();
+    translate(width / 2 - w, height / 2 + i * h);
+    if (n == 24) {
+      rotate(15);
+    }
+    polygon(0, 0, n);
+    pop();
   }
-  polygon(0, 0, n);
-  pop();
-  push();
-  translate(width / 2 - w, height / 2 - h);
-  if (n == 24) {
-    rotate(15);
-  }
-  polygon(0, 0, n);
-  pop();
-  push();
-  translate(width / 2 - w, height / 2 + 3 * h);
-  if (n == 24) {
-    rotate(15);
-  }
-  polygon(0, 0, n);
-  pop();
-  push();
-  translate(width / 2 - w, height / 2 - 3 * h);
-  if (n == 24) {
-    rotate(15);
-  }
-  polygon(0, 0, n);
-  pop();
 
   stroke(30);
   if (dist(mouseX, 0, width / 2 + 2 * w, 0) <= 100 * sin(60)) {
@@ -168,27 +130,15 @@ function mainShow(n) {
       stroke(z4);
     }
   }
-  push();
-  translate(width / 2 + 2 * w, height / 2 - 2 * h);
-  if (n == 24) {
-    rotate(15);
+  for (var i = -4; i <= 4; i += 2) {
+    push();
+    translate(width / 2 + 2 * w, height / 2 + i * h);
+    if (n == 24) {
+      rotate(15);
+    }
+    polygon(0, 0, n);
+    pop();
   }
-  polygon(0, 0, n);
-  pop();
-  push();
-  translate(width / 2 + 2 * w, height / 2 + 2 * h);
-  if (n == 24) {
-    rotate(15);
-  }
-  polygon(0, 0, n);
-  pop();
-  push();
-  translate(width / 2 + 2 * w, height / 2);
-  if (n == 24) {
-    rotate(15);
-  }
-  polygon(0, 0, n);
-  pop();
 
   stroke(30);
   if (dist(mouseX, 0, width / 2 - 2 * w, 0) <= 100 * sin(60)) {
@@ -200,27 +150,15 @@ function mainShow(n) {
       stroke(z5);
     }
   }
-  push();
-  translate(width / 2 - 2 * w, height / 2 - 2 * h);
-  if (n == 24) {
-    rotate(15);
+  for (var i = -4; i <= 4; i += 2) {
+    push();
+    translate(width / 2 - 2 * w, height / 2 + i * h);
+    if (n == 24) {
+      rotate(15);
+    }
+    polygon(0, 0, n);
+    pop();
   }
-  polygon(0, 0, n);
-  pop();
-  push();
-  translate(width / 2 - 2 * w, height / 2 + 2 * h);
-  if (n == 24) {
-    rotate(15);
-  }
-  polygon(0, 0, n);
-  pop();
-  push();
-  translate(width / 2 - 2 * w, height / 2);
-  if (n == 24) {
-    rotate(15);
-  }
-  polygon(0, 0, n);
-  pop();
 
   //expanding
   stroke(30);
@@ -233,20 +171,15 @@ function mainShow(n) {
       stroke(z6);
     }
   }
-  push();
-  translate(width / 2 - 3 * w, height / 2 - h);
-  if (n == 24) {
-    rotate(15);
+  for (var i = -5; i <= 5; i += 2) {
+    push();
+    translate(width / 2 - 3 * w, height / 2 + i * h);
+    if (n == 24) {
+      rotate(15);
+    }
+    polygon(0, 0, n);
+    pop();
   }
-  polygon(0, 0, n);
-  pop();
-  push();
-  translate(width / 2 - 3 * w, height / 2 + h);
-  if (n == 24) {
-    rotate(15);
-  }
-  polygon(0, 0, n);
-  pop();
 
   stroke(30);
   if (dist(mouseX, 0, width / 2 + 3 * w, 0) <= 100 * sin(60)) {
@@ -258,20 +191,15 @@ function mainShow(n) {
       stroke(z7);
     }
   }
-  push();
-  translate(width / 2 + 3 * w, height / 2 - h);
-  if (n == 24) {
-    rotate(15);
+  for (var i = -5; i <= 5; i += 2) {
+    push();
+    translate(width / 2 + 3 * w, height / 2 + i * h);
+    if (n == 24) {
+      rotate(15);
+    }
+    polygon(0, 0, n);
+    pop();
   }
-  polygon(0, 0, n);
-  pop();
-  push();
-  translate(width / 2 + 3 * w, height / 2 + h);
-  if (n == 24) {
-    rotate(15);
-  }
-  polygon(0, 0, n);
-  pop();
 
   stroke(30);
   if (dist(mouseX, 0, width / 2 + 4 * w, 0) <= 100 * sin(60)) {
@@ -283,13 +211,15 @@ function mainShow(n) {
       stroke(z9);
     }
   }
-  push();
-  translate(width / 2 + 4 * w, height / 2);
-  if (n == 24) {
-    rotate(15);
+  for (var i = -4; i <= 4; i += 2) {
+    push();
+    translate(width / 2 + 4 * w, height / 2 + i * h);
+    if (n == 24) {
+      rotate(15);
+    }
+    polygon(0, 0, n);
+    pop();
   }
-  polygon(0, 0, n);
-  pop();
 
   stroke(30);
   if (dist(mouseX, 0, width / 2 - 4 * w, 0) <= 100 * sin(60)) {
@@ -301,11 +231,133 @@ function mainShow(n) {
       stroke(z8);
     }
   }
-  push();
-  translate(width / 2 - 4 * w, height / 2);
-  if (n == 24) {
-    rotate(15);
+  for (var i = -4; i <= 4; i += 2) {
+    push();
+    translate(width / 2 - 4 * w, height / 2 + i * h);
+    if (n == 24) {
+      rotate(15);
+    }
+    polygon(0, 0, n);
+    pop();
   }
-  polygon(0, 0, n);
-  pop();
+
+  stroke(30);
+  if (dist(mouseX, 0, width / 2 - 5 * w, 0) <= 100 * sin(60)) {
+    z10 = 200;
+    stroke(z10);
+  } else {
+    if (z10 >= 30) {
+      z10 = z10 * 0.995;
+      stroke(z10);
+    }
+  }
+  for (var i = -5; i <= 5; i += 2) {
+    push();
+    translate(width / 2 - 5 * w, height / 2 + i * h);
+    if (n == 24) {
+      rotate(15);
+    }
+    polygon(0, 0, n);
+    pop();
+  }
+
+  stroke(30);
+  if (dist(mouseX, 0, width / 2 + 5 * w, 0) <= 100 * sin(60)) {
+    z11 = 200;
+    stroke(z11);
+  } else {
+    if (z11 >= 30) {
+      z11 = z11 * 0.995;
+      stroke(z11);
+    }
+  }
+  for (var i = -5; i <= 5; i += 2) {
+    push();
+    translate(width / 2 + 5 * w, height / 2 + i * h);
+    if (n == 24) {
+      rotate(15);
+    }
+    polygon(0, 0, n);
+    pop();
+  }
+
+  stroke(30);
+  if (dist(mouseX, 0, width / 2 + 6 * w, 0) <= 100 * sin(60)) {
+    z12 = 200;
+    stroke(z12);
+  } else {
+    if (z12 >= 30) {
+      z12 = z12 * 0.995;
+      stroke(z12);
+    }
+  }
+  for (var i = -4; i <= 4; i += 2) {
+    push();
+    translate(width / 2 + 6 * w, height / 2 + i * h);
+    if (n == 24) {
+      rotate(15);
+    }
+    polygon(0, 0, n);
+    pop();
+  }
+
+  stroke(30);
+  if (dist(mouseX, 0, width / 2 - 6 * w, 0) <= 100 * sin(60)) {
+    z13 = 200;
+    stroke(z13);
+  } else {
+    if (z13 >= 30) {
+      z13 = z13 * 0.995;
+      stroke(z13);
+    }
+  }
+  for (var i = -4; i <= 4; i += 2) {
+    push();
+    translate(width / 2 - 6 * w, height / 2 + i * h);
+    if (n == 24) {
+      rotate(15);
+    }
+    polygon(0, 0, n);
+    pop();
+  }
+
+  stroke(30);
+  if (dist(mouseX, 0, width / 2 + 7 * w, 0) <= 100 * sin(60)) {
+    z14 = 200;
+    stroke(z14);
+  } else {
+    if (z14 >= 30) {
+      z14 = z14 * 0.995;
+      stroke(z14);
+    }
+  }
+  for (var i = -5; i <= 5; i += 2) {
+    push();
+    translate(width / 2 + 7 * w, height / 2 + i * h);
+    if (n == 24) {
+      rotate(15);
+    }
+    polygon(0, 0, n);
+    pop();
+  }
+
+  stroke(30);
+  if (dist(mouseX, 0, width / 2 - 7 * w, 0) <= 100 * sin(60)) {
+    z15 = 200;
+    stroke(z15);
+  } else {
+    if (z15 >= 30) {
+      z15 = z15 * 0.995;
+      stroke(z15);
+    }
+  }
+  for (var i = -5; i <= 5; i += 2) {
+    push();
+    translate(width / 2 - 7 * w, height / 2 + i * h);
+    if (n == 24) {
+      rotate(15);
+    }
+    polygon(0, 0, n);
+    pop();
+  }
 }
