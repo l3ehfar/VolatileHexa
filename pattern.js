1892c951abbd1be67d8d8159ad54e259a020eb18
l3ehfar <behnooshmzd@gmail.com>
2019-06-28 12:57:58 +0430

    fix web version step2

--- pattern.js
+++ pattern.js
@@ -100,7 +100,7 @@ function pattern(w, h) {
     step4 = map(timer, 106500.0, 108000.0, 65, 40);
   }
   for (var i = -10; i <= 10; i++) {
-    if (vol >= 0.25) {
+    if (vol >= 0.65 && vol <= 0.89) {
       var r = int(random(-7, 7));
       if (i == r) {
         fill(step1);
@@ -302,7 +302,7 @@ function pattern(w, h) {
   pop();
   /*--------------------number2------------------------*/
   push();
-  if (vol >= 0.5 && vol <= 0.54) {
+  if (vol >= 0.5 && vol <= 0.64) {
     frameRate(30);
     note_i_2 = int(random([-7, -5]));
 
@@ -386,7 +386,7 @@ function pattern(w, h) {
   pop();
   /*------------------------------number3----------------------------*/
   push();
-  if (vol >= 0.55 && vol <= 0.79) {
+  if (vol >= 0.65 && vol <= 0.89) {
     frameRate(30);
     note_i_3 = int(random([6, -6]));
 
